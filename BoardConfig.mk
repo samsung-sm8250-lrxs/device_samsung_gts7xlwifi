9f86b64dbfb63dc56890a1dc70d853d19aa0ce0d
Lunarixus <Nathan@lunarixus.dev>
2024-01-18 23:37:53 +0000

    gts7xlwifi: Initial Skeleton Tree
    Signed-off-by: Lunarixus <Nathan@lunarixus.dev>

---
+++ BoardConfig.mk
@@ -0,0 +1,31 @@
+#
+# Copyright (C) 2024 Lunarixus.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+DEVICE_PATH := device/samsung/gts7xlwifi
+
+# Partitions
+BOARD_FLASH_BLOCK_SIZE := 262144
+BOARD_SUPER_PARTITION_SIZE := 10171187200
+BOARD_SUPER_PARTITION_GROUPS := samsung_dynamic_partitions
+BOARD_SAMSUNG_DYNAMIC_PARTITIONS_PARTITION_LIST := product system
+BOARD_SAMSUNG_DYNAMIC_PARTITIONS_SIZE := 10171167200
+
+BOARD_BOOTIMAGE_PARTITION_SIZE := 71303168
+BOARD_CACHEIMAGE_PARTITION_SIZE := 419430400
+BOARD_DTBOIMG_PARTITION_SIZE := 10485760
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 86888448
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 115026669568
+
+include device/samsung/sm8250-common/BoardConfig-common.mk
---
+++ gts7xlwifi.mk
@@ -0,0 +1,24 @@
+#
+# Copyright (C) 2024 Lunarixus.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+$(call inherit-product, device/samsung/sm8250-common/hentai_common.mk)
+$(call inherit-product, device/samsung/gts7xlwifi/device.mk)
+
+PRODUCT_NAME := gts7xlwifi
+PRODUCT_DEVICE := gts7xlwifi
+PRODUCT_MODEL := Galaxy Tab S7+
+PRODUCT_BRAND := samsung
+PRODUCT_MANUFACTURER := Samsung
